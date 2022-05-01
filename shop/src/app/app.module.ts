62646e5bc78e977c085b48299b4934862891e207
Chazz <czcarrizales@yahoo.com>
2022-04-30 22:40:02 -0700

    add shop-main, shop-category, shop-list components

--- shop/src/app/app.module.ts
+++ shop/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ShopMainComponent } from './shop-main/shop-main.component';
+import { ShopCategoryComponent } from './shop-category/shop-category.component';
+import { ShopListComponent } from './shop-list/shop-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ShopMainComponent,
+    ShopCategoryComponent,
+    ShopListComponent
   ],
   imports: [
     BrowserModule,
